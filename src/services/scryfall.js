f679b38c973af6631af2d62b3a4d1cac7641069d
Lucas-AV7 <lucas.av7@yahoo.com>
2021-03-23 23:48:22 -0300

    New api routes

--- src/services/scryfall.js
+++ src/services/scryfall.js
@@ -7,3 +7,15 @@ export const instance = axios.create({
 export const scryFallSets = () => {
   return instance.get("/sets");
 };
+
+export const scryFallRandomCard = () => {
+  return instance.get("/cards/random");
+};
+
+export const scryFallSearchCard = (text) => {
+  return instance.get(`/cards/search?q=${text}`);
+};
+
+export const scryFallAutoComplete = (text) => {
+  return instance.get(`/cards/autocomplete?q=${text}`);
+};
