33fa6f391b3bbb22a9e40d6eae22f5aa7b224cef
Lucas-AV7 <lucas.av7@yahoo.com>
2021-03-25 16:31:56 -0300

    Autocomplete api error handler

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,9 +2,15 @@ import { scryFallAutoComplete } from "@/services/scryfall";
 
 export const actions = {
   fetchAutocomplete({ commit }, text) {
-    scryFallAutoComplete(text).then((r) => {
-      commit("changeAutocomplete", r.data.data);
-    });
+    scryFallAutoComplete(text)
+      .then((response) => {
+        commit("changeAutocomplete", response.data.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      });
   },
   addUsersDeck({ commit, state }, deck) {
     console.log(state, deck);
